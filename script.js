64c908ad71f960a900919c87a5007de7e06f8445
Pablo Volonté <pvolonte@gmail.com>
2022-04-18 22:15:33 -0300

    Actualizacion

---
+++ script.js
@@ -0,0 +1,7 @@
+let nombreUsuario = prompt("Ingrese nombre de Usuario");
+if(nombreUsuario == ""){
+    alert("No ingresarte Nombre de Usuario");
+}
+else{
+    alert("Ingresaste" + " " + nombreUsuario);
+}
