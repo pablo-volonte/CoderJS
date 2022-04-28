152a6c45d34c2c1989563a6c2ee57b40f99fd169
Pablo Volonté <pvolonte@gmail.com>
2022-04-27 23:23:23 -0300

    Presentacion trabajo Volonté Pablo

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let nombreUsuario = prompt("Ingrese nombre de Usuario");
-if(nombreUsuario == ""){
-    alert("No ingresarte Nombre de Usuario");
+let Verdadero = true;
+
+while (Verdadero) {
+	let nombreUsuario = prompt("Ingrese su Nombre");
+	let apellidoUsuario = prompt("Ahora ingrese su Apellido");
+	if (nombreUsuario == "" || apellidoUsuario == "") {
+		confirm("¡ATENCION! Ingrese la informacíon Completa");
+	} else {
+		Verdadero = false;
+	}
 }
-else{
-    alert("Ingresaste" + " " + nombreUsuario);
-}
+alert("Felicitaciones, Que seas Bienvenido");
+
+// else {
+// 	confirm("Ingresaste" + " " + nombreYapellido + " " + "¿Es correcto?");
+// }
+/*while (verdadero) {
+	
+	if (apellidoUsuario == "") {
+		confirm("¡ATENCION! Ingrese su Nombre");
+	}
+	while (Verdadero) {
+	let apellidoUsuario = prompt("Ahora ingrese su Apellido");
+	if (apellidoUsuario == "") {
+		confirm("¡ATENCION! Ingrese su Nombre");
+	} else {
+		Verdadero = false;
+	}
+}*/
