11f7160b424fffcca1085420d9d49a3751966406
Pablo Volonté <pvolonte@gmail.com>
2022-05-03 01:48:17 -0300

    entrega simulador interactivo

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 let Verdadero = true;
-while (Verdadero) {
+/*while (Verdadero) {
 	let nombreUsuario = prompt("Ingrese su Nombre");
 	let apellidoUsuario = prompt("Ahora ingrese su Apellido");
 	if (nombreUsuario == "" || apellidoUsuario == "") {
 		confirm("¡ATENCION! Ingrese la informacíon Completa");
 	} else {
-		Verdadero = false;
+		Verdadero=false;
 		let nombreCompleto = nombreUsuario + " " + apellidoUsuario;
 		alert("Felicitaciones" + " " + nombreCompleto + " " + ",Bienvenido");
+		console.log("El nombre completo es" + " " + nombreCompleto);
+	}
+} */
+let numeroElegido = parseInt(prompt("ingrese un numero del 1 al 10"));
+let numeroPedido = parseInt(prompt("Pida un numero del 1 al 10 y colóquelo"));
+
+function ingresoNum(pedido) {
+	console.log(pedido);
+}
+ingresoNum(numeroPedido);
+while (Verdadero) {
+	if (numeroElegido == numeroPedido && numeroElegido < 11) {
+		Verdadero = false;
+		alert("Bravo!! Acertaste con el número");
+	} else {
+		numeroPedido = parseInt(prompt("Pida otro numero "));
+		alert(numeroPedido + " " + "No es el número elegido, siga intentando");
 	}
 }
