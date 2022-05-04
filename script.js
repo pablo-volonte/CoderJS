2ffc80ac9e58f325a3f7e2cf4dfda653767ba2c4
Pablo Volonté <pvolonte@gmail.com>
2022-05-04 15:21:57 -0300

    Actualizacion entrega Desafio entregable 1 Volonté

--- script.js
+++ script.js
@@ -10,8 +10,12 @@ let Verdadero = true;
 		alert("Felicitaciones" + " " + nombreCompleto + " " + ",Bienvenido");
 		console.log("El nombre completo es" + " " + nombreCompleto);
 	}
-} */
-let numeroElegido = parseInt(prompt("ingrese un numero del 1 al 10"));
+}
+
+
+
+*/
+/* let numeroElegido = parseInt(prompt("ingrese un numero del 1 al 10"));
 let numeroPedido = parseInt(prompt("Pida un numero del 1 al 10 y colóquelo"));
 
 function ingresoNum(pedido) {
@@ -27,3 +31,32 @@ while (Verdadero) {
 		alert(numeroPedido + " " + "No es el número elegido, siga intentando");
 	}
 }
+ */
+const iva = 1.21;
+let producto;
+let precioProducto;
+
+const ingresoProducto = () => {
+	do {
+		producto = prompt("Ingrese por favor un nuevo producto");
+	} while (producto === "");
+	console.log(`El producto que ingresó es: ${producto}`);
+};
+
+const ingresoPrecio = () => {
+	do {
+		precioProducto = parseFloat(
+			prompt(`Asigne el costo del producto "${producto}"`)
+		);
+	} while (isNaN(precioProducto) || precioProducto === "");
+	console.log(`El precio de ${producto} es $ ${precioProducto}`);
+};
+
+const precioFinal = () => {
+	let valorfinal = precioProducto * iva;
+	alert(`el precio final de ${producto} es ${valorfinal}`);
+};
+
+ingresoProducto();
+ingresoPrecio();
+precioFinal();
